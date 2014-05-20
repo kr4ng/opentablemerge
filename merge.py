d2d06486d133472870f68f91bcd0e12441415b16
kr4ng <steven.m.simoni@gmail.com>
2014-05-20 16:07:54 -0700

    d

--- merge.py
+++ merge.py
@@ -4,39 +4,39 @@ import urllib3
 from flask import Flask, request, json
 
 
-#from flask.ext import restful
-#from flask.ext.jsonpify import jsonify
+from flask.ext import restful
+from flask.ext.jsonpify import jsonify
 
 app = Flask(__name__)
 #api = restful.Api(app)
 app.debug = True
 
-#class merge(restful.Resource):
-@app.route('/<ID>', methods=['POST'])
-def merge(self):
-    #Establish Connection, create the client handler
-    #This is the msc.com instance connection
-    #You see I own this town, you best not come around if you
-    #If you wanna mess around like that, that's just how it is.
-    jsonRequest = request.get_json(force = True)
-    ID = jsonRequest['ID']
+class merge(restful.Resource):
+#@app.route('/<ID>', methods=['POST'])
+    def merge(self):
+        #Establish Connection, create the client handler
+        #This is the msc.com instance connection
+        #You see I own this town, you best not come around if you
+        #If you wanna mess around like that, that's just how it is.
+        jsonRequest = request.get_json(force = True)
+        ID = jsonRequest['ID']
 
-    client = suds_marketo.Client(soap_endpoint='https://970-WBY-466.mktoapi.com/soap/mktows/2_3',
-                                 user_id='opentable1_62824687530E8A604131D1',
-                                 encryption_key='5335137655137532553300EE88AA661244113492BE69')
+        client = suds_marketo.Client(soap_endpoint='https://970-WBY-466.mktoapi.com/soap/mktows/2_3',
+                                     user_id='opentable1_62824687530E8A604131D1',
+                                     encryption_key='5335137655137532553300EE88AA661244113492BE69')
 
-    NewContactFromSFDC = str(ID)
-    
-    #get the leads from Marketo
-    lead = client.get_lead_IDNUM(NewContactFromSFDC)
-    for i in range(0,len(lead.leadRecordList.leadRecord[0].leadAttributeList[0])):
-        if 'mKTOLeadID' == lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrName:
-            originalmarketoid = lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrValue
-    client.merge_leads(NewContactFromSFDC,originalmarketoid)
+        NewContactFromSFDC = str(ID)
+        
+        #get the leads from Marketo
+        lead = client.get_lead_IDNUM(NewContactFromSFDC)
+        for i in range(0,len(lead.leadRecordList.leadRecord[0].leadAttributeList[0])):
+            if 'mKTOLeadID' == lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrName:
+                originalmarketoid = lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrValue
+        client.merge_leads(NewContactFromSFDC,originalmarketoid)
 
-    return json.jsonify(Message = "",ResultCode = 0)
+        return json.jsonify(Message = "",ResultCode = 0)
 
-#api.add_resource(merge, '/<int:ID>')
+api.add_resource(merge, '/<int:ID>')
 
 if __name__ == '__main__':
     #app.run(debug=True)
