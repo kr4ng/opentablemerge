1a674e02e09e18556636c04302b2565ac4d3a014
kr4ng <steven.m.simoni@gmail.com>
2014-05-20 21:44:12 -0700

    d

--- merge.py
+++ merge.py
@@ -9,18 +9,10 @@ from flask.ext.jsonpify import jsonify
 
 app = Flask(__name__)
 api = restful.Api(app)
-app.debug = True
+#app.debug = True
 
 class merge(restful.Resource):
-#@app.route('/<ID>', methods=['POST'])
     def get(self, ID):
-        #Establish Connection, create the client handler
-        #This is the msc.com instance connection
-        #You see I own this town, you best not come around if you
-        #If you wanna mess around like that, that's just how it is.
-        #jsonRequest = request.get_json(force = True)
-        #ID = jsonRequest['ID']
-
         client = suds_marketo.Client(soap_endpoint='https://970-WBY-466.mktoapi.com/soap/mktows/2_3',
                                      user_id='opentable1_62824687530E8A604131D1',
                                      encryption_key='5335137655137532553300EE88AA661244113492BE69')
@@ -32,28 +24,10 @@ class merge(restful.Resource):
             if 'mKTOLeadID' == lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrName:
                 originalmarketoid = lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrValue
         client.merge_leads(NewContactFromSFDC,originalmarketoid)
-
-        #return json.jsonify(Message = "",ResultCode = 0)
         return None
 
 api.add_resource(merge, '/<int:ID>')
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    client = suds_marketo.Client(soap_endpoint='https://970-WBY-466.mktoapi.com/soap/mktows/2_3',
-                                     user_id='opentable1_62824687530E8A604131D1',
-                                     encryption_key='5335137655137532553300EE88AA661244113492BE69')
-    lead = client.get_lead_IDNUM('543145')
-    for i in range(0,len(lead.leadRecordList.leadRecord[0].leadAttributeList[0])):
-        if 'mKTOLeadID' == lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrName:
-            originalmarketoid = lead.leadRecordList.leadRecord[0].leadAttributeList[0][i].attrValue
-    client.merge_leads('543145',originalmarketoid)
-    '''
-    try:
-        originalmarketoid = lead.originalmarketoid
-        #make sure the original lead exists
-        leadtomerge = client.get_lead(originalmarketoid)
-        client.merge_leads(ID, orignalmarketoid)
-    except:
-        return
-    '''
+    #for debugging
